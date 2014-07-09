004499ee2cb7e4362e7012787b5bc2165c28e7fa
lukeisme <lukeisme2013@gmail.com>
2014-07-09 16:12:22 +0800

    v1.1

--- WebRoot/src/player.js
+++ WebRoot/src/player.js
@@ -436,7 +436,7 @@ KISSY.add(function (S, Node, Anim, XTemplate, IO, dd, ListTpl, TypeTpl) {
         container.delegate('click', '.t-singer', function (ev) {
             $('.search-result').css('display', 'none');
             $('.text-body').css('display', 'block');
-            $('.detail-info').css('transform', 'translate3D(0, 340px, 0)');
+            $('.detail-info').css('transform', 'rotateX(0)');
             ev.halt();
         });
         this.typeListenTimes++;
@@ -595,6 +595,13 @@ KISSY.add(function (S, Node, Anim, XTemplate, IO, dd, ListTpl, TypeTpl) {
                     $('.t-' + listType + ' .t-list li').remove();
                     self.musicData[listType] = data;
                     self.mkTypeList(listType);
+                    if ($('.t-' + listType + ' .t-list').hasClass('fade-in')) {
+                        $('.t-' + listType + ' .t-list').removeClass('fade-in');
+                        $('.t-' + listType + ' .t-list').addClass('fade-in2');
+                    } else {
+                        $('.t-' + listType + ' .t-list').removeClass('fade-in2');
+                        $('.t-' + listType + ' .t-list').addClass('fade-in');
+                    }
                     prePage.css('display', 'block');
                     if (index === 1)
                         prePage.addClass('disable');
@@ -638,7 +645,7 @@ KISSY.add(function (S, Node, Anim, XTemplate, IO, dd, ListTpl, TypeTpl) {
                     } else {
                         $('.t-' + listType + ' .nextPage').css('display', 'block');
                     }
-
+                    $('.t-' + listType + ' .t-list').addClass('fade-in');
                 },
                 error: function (m, io) {
                     console.log(m);
@@ -725,19 +732,19 @@ KISSY.add(function (S, Node, Anim, XTemplate, IO, dd, ListTpl, TypeTpl) {
                     for (var i = 0; i < data["歌曲"].length; i++) {
                         if (resultCount >= 5)
                             break;
-                        mulStr += '<li><span class = "s-info">' + data["歌曲"][i].songName + '</span><span class = "s-type">歌曲</span></li>';
+                        mulStr += '<li class = "hint-item"><span class = "s-info">' + data["歌曲"][i].songName + '</span><span class = "s-type">歌曲</span></li>';
                         resultCount++;
                     }
                     for (var i = 0; i < data["歌手"].length; i++) {
                         if (resultCount >= 5)
                             break;
-                        mulStr += '<li><span class = "s-info">' + data["歌手"][i].singerName + '</span><span class = "s-type">歌手</span></li>';
+                        mulStr += '<li class = "hint-item"><span class = "s-info">' + data["歌手"][i].singerName + '</span><span class = "s-type">歌手</span></li>';
                         resultCount++;
                     }
                     for (var i = 0; i < data["专辑"].length; i++) {
                         if (resultCount >= 5)
                             break;
-                        mulStr += '<li><span class = "s-info">' + data["专辑"][i].albumName + '</span><span class = "s-type">专辑</span></li>';
+                        mulStr += '<li class = "hint-item"><span class = "s-info">' + data["专辑"][i].albumName + '</span><span class = "s-type">专辑</span></li>';
                         resultCount++;
                     }
                     $('.search-hint ul').append(mulStr);
@@ -749,6 +756,13 @@ KISSY.add(function (S, Node, Anim, XTemplate, IO, dd, ListTpl, TypeTpl) {
                 dataType: "json"
             });
         });
+        /*console.log($('.search-hint'));
+        $('.wrapper').delegate('click', '.hint-item', function (ev) {
+            console.log('click hint');
+            $('.search input').val($(ev.currentTarget).one('.s-info').text());
+            searchKeyWord();
+            ev.halt();
+        });*/
 
         function searchKeyWord() {
             var key = searchInput.val();
@@ -771,7 +785,11 @@ KISSY.add(function (S, Node, Anim, XTemplate, IO, dd, ListTpl, TypeTpl) {
                     searchResult = data;
                     textBody.css('display', 'none');
                     $('.search-result').css('display', 'block');
-                    $('.detail-info').css('transform', 'translate3D(0, 340px, 0)');
+                    $('.detail-info').css({
+                        'transform': 'rotateX(0)',
+                        'z-index': 200
+                    });
+                    $('.p-panel').css('z-index', 0);
                 },
                 error: function (m, io) {
                     console.log(m);
@@ -799,7 +817,7 @@ KISSY.add(function (S, Node, Anim, XTemplate, IO, dd, ListTpl, TypeTpl) {
             ev.halt();
         });
         $('.search-result>ul').delegate('click', '.t-singer', function (ev) {
-            $('.detail-info').css('transform', 'translate3D(0, 340px, 0)');
+            $('.detail-info').css('transform', 'rotateX(90deg)');
             ev.halt();
         });
 
@@ -808,7 +826,7 @@ KISSY.add(function (S, Node, Anim, XTemplate, IO, dd, ListTpl, TypeTpl) {
             switch (ev.keyCode) {
             case 13:
                 /*enter*/
-                if($('.search-hint .selected').length !== 0)
+                if ($('.search-hint .selected').length !== 0)
                     $('.search input').val($(hintItem[self.hintIndex]).one('.s-info').text());
                 searchKeyWord();
                 ev.halt();
@@ -841,7 +859,11 @@ KISSY.add(function (S, Node, Anim, XTemplate, IO, dd, ListTpl, TypeTpl) {
     Player.prototype.handleDetail = function () {
         var mainEl = $('.detail-info');
         $('.close').on('click', function () {
-            mainEl.css('transform', 'translate3D(0, 0, 0)');
+            mainEl.css({
+                'transform': 'rotateX(90deg)',
+                'z-index': 0
+            });
+            $('.p-panel').css('z-index', 100);
         });
     };
 
